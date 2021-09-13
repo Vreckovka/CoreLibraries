ff8f340742c0ded8fbbe885455568961a1d8535e
Roman Pecho <roman.pecho@colosseoeas.com>
2021-09-13 10:06:55 +0200

    hgf

--- VCore/Controls/PlayableWrapPanel.cs
+++ VCore/Controls/PlayableWrapPanel.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace VCore.Controls
 {
+  [StyleTypedProperty(Property = "ItemContainerStyle", StyleTargetType = typeof(PlayableWrapPanelItem))]
   public class PlayableWrapPanel : ItemsControl
   {
 
     public PlayableWrapPanel()
     {
 
+    }
+
+    protected override DependencyObject GetContainerForItemOverride()
+    {
+      return new PlayableWrapPanelItem();
     }
 
     protected override void OnItemsSourceChanged(IEnumerable oldValue, IEnumerable newValue)
