7d1fc5a0ec29028f91467f00e6a0abbed01f55b8
Roman Pecho <roman.pecho@colosseoeas.com>
2021-10-07 09:41:12 +0200

    fds

---
+++ VCore.Standard/AsyncProcess.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Threading.Tasks;
+
+namespace VCore.Standard
+{
+  public class AsyncProcess<TResult>
+  {
+    private ReplaySubject<int> internalProcessedCountSubject = new ReplaySubject<int>(1);
+
+    public Task<TResult> Process { get; set; }
+    public int InternalProcessesCount { get; set; }
+
+    #region ProcessedCount
+
+    private int processedCount;
+    public int ProcessedCount
+    {
+      get
+      {
+        return processedCount;
+      }
+      set
+      {
+        if (value != processedCount)
+        {
+          processedCount = value;
+          internalProcessedCountSubject.OnNext(processedCount);
+        }
+      }
+    }
+
+    #endregion
+
+    public IObservable<int> OnInternalProcessedCountChanged
+    {
+      get
+      {
+        return internalProcessedCountSubject.AsObservable();
+      }
+    }
+  }
+}
