d6d28ed86748db3671e9d36fa1703b563ba5196e
Roman Pecho <roman.pecho@colosseoeas.com>
2021-10-21 08:42:16 +0200

    hgf

--- VCore.Standard/ViewModels/ViewModel.cs
+++ VCore.Standard/ViewModels/ViewModel.cs
@@ -63,4 +63,35 @@ namespace VCore.Standard
 
     #endregion Properties
   }
+
+  public abstract class SelectableViewModel<TModel> : ViewModel<TModel>
+  {
+    protected SelectableViewModel(TModel model) : base(model)
+    {
+    }
+
+    private bool isSelected;
+
+    public bool IsSelected
+    {
+      get
+      {
+        return isSelected;
+      }
+      set
+      {
+        if (value != isSelected)
+        {
+          isSelected = value;
+          OnSelectionChanged(value);
+          RaisePropertyChanged();
+        }
+      }
+    }
+
+    protected virtual void OnSelectionChanged(bool newValue)
+    {
+
+    }
+  }
 }
